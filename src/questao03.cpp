d0e810e31e08211645191206dbd6c8c60163dc2e
tiago-sb <belatiago72@gmail.com>
2023-04-18 14:28:26 -0300

    Update questao03.cpp

--- src/questao03.cpp
+++ src/questao03.cpp
@@ -1,6 +1,6 @@
 /*
-Crie um algoritmo que leia um valor inteiro do teclado na variável a e atribua esse valor a uma outra variável,
-b. Mostre na tela os valores das duas variáveis.
+Crie um algoritmo que leia um valor inteiro do teclado na variavel a e atribua esse valor a uma outra variavel,
+b. Mostre na tela os valores das duas variaveis.
 */
 #include <iostream>
 
