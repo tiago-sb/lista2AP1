76459712b2450a01f9024432f9bb3e1cb1125e68
tiago-sb <belatiago72@gmail.com>
2023-04-18 14:21:31 -0300

    Create questao03.cpp

---
+++ src/questao03.cpp
@@ -0,0 +1,13 @@
+/*
+Crie um algoritmo que leia um valor inteiro do teclado na variável a e atribua esse valor a uma outra variável,
+b. Mostre na tela os valores das duas variáveis.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
