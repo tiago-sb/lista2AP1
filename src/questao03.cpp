268b2f941b8019ba0b8b491d6c7d36e6eeec314b
tiago-sb <belatiago72@gmail.com>
2023-04-18 14:27:56 -0300

    Update questao03.cpp

--- src/questao03.cpp
+++ src/questao03.cpp
@@ -7,7 +7,16 @@ b. Mostre na tela os valores das duas variáveis.
 using namespace std;
 
 int main(){
-
+    int a, b;
+    cout << "Insira um numero inteiro: ";
+    cin >> a;
+    cout << "Insira outro numero inteiro: ";
+    cin >> b;
+    cout << "Valor de 'A' Pre-atribuicao: " << a << endl;
+    cout << "Valor de 'B': " << b << endl;
+    a = b;
+    cout << "Valor de 'A' Pos-atribuicao: " << a << endl;
+    cout << "Valor de 'B': " << b << endl;
 
     return 0;
 }
